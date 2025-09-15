5755647a8b25e7739e871b1acff4cffc2b05d23b
Jeff Martin <jeff@reportmill.com>
2025-09-15 11:44:14 -0500

    Add SnapCodeAll.java to run SnapCode application

---
+++ jbang/SnapCodeAll.java
@@ -0,0 +1,10 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//REPOS https://reportmill.com/maven
+//DEPS com.reportmill:SnapCodeAll:2025.03
+
+public class SnapCodeAll {
+
+    public static void main(String... args) {
+        snapcode.app.App.main(new String[0]);
+    }
+}
