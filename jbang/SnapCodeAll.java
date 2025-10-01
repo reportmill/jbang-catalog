19eba6b375a9d0a1bf121f1a2e23f04c6ec4e645
Jeff Martin <jeff@reportmill.com>
2025-10-01 10:36:42 -0500

    SnapCodeAll: Update to September release

--- jbang/SnapCodeAll.java
+++ jbang/SnapCodeAll.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //REPOS https://reportmill.com/maven
-//DEPS com.reportmill:SnapCodeAll:2025.03
+//DEPS com.reportmill:SnapCodeAll:2025.09
 
 public class SnapCodeAll {
 
